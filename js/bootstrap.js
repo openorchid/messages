1dab15823b35ade6afebf6d715f9301fd973a2dc
mortada-killer0 <mcsplayer914@gmail.com>
2023-11-21 11:13:32 +0300

    Path fixing and SW registering

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,19 +1,26 @@
 !(function (exports) {
   'use strict';
 
-  window.addEventListener('DOMContentLoaded', function () {
-    // Initialize
-    SpatialNavigation.init();
+  // Check if the browser supports service workers
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('./sw.js')
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.log('Service Worker registration failed:', error);
+        });
+    });
+  }
 
-    // Define navigable elements (anchors and elements with "focusable" class).
+  window.addEventListener('DOMContentLoaded', function () {
+    SpatialNavigation.init();
     SpatialNavigation.add({
       selector: '.tablist a, .tablist button, .tablist .lists ul li, .visible a, .visible button, .visible .lists ul li, .visible .lists ul li input'
     });
-
-    // Make the *currently existing* navigable elements focusable.
     SpatialNavigation.makeFocusable();
-
-    // Focus the first navigable element.
     SpatialNavigation.focus();
   });
 })(window);
